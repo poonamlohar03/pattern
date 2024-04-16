2ebdc9cd5ebc04681516f0901202b61e73d453ec
poonamlohar03 <bansilallohar1112@gmail.com>
2024-04-16 07:06:25 +0530

    added url

---
+++ patternright1.java
@@ -0,0 +1,17 @@
+public class patternright1{
+public static void main(String[] args) {
+    int i,j,n=6;
+	for(i=0;i<n;i++){
+	for(j=0;j<=i;j++){
+		System.out.print("* ");
+}
+System.out.println(" ");
+
+
+
+}
+
+}
+
+
+}
